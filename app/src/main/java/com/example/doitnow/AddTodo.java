3b0112d6f918302768231a0901134205c7511d86
Anastasija <andonova.anastasija98@gmail.com>
2022-01-08 23:48:37 +0100

    add a google map view in AddTodo.java

--- app/src/main/java/com/example/doitnow/AddTodo.java
+++ app/src/main/java/com/example/doitnow/AddTodo.java
@@ -1,10 +1,19 @@
 package com.example.doitnow;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 import com.example.doitnow.adapters.TodosRecyclerAdapter;
@@ -13,6 +22,12 @@ import com.example.doitnow.databinding.TodoAddBinding;
 import com.example.doitnow.db.AppDatabase;
 import com.example.doitnow.models.TodoItem;
 import com.example.doitnow.utils.DatabaseInitializer;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,12 +42,42 @@ public class AddTodo extends Fragment {
     TodosRecyclerAdapter adapter;
     List<TodoItem> todosList;
 
+    private static final int LOCATION_MIN_UPDATE_TIME = 10;
+    private static final int LOCATION_MIN_UPDATE_DISTANCE = 1000;
+    private MapView mapView;
+    private GoogleMap googleMap;
+    private Location location = null;
+
+    private LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            drawMarker(location, getText(R.string.your_location).toString());
+            locationManager.removeUpdates(locationListener);
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
+    };
+
+    private LocationManager locationManager;
+
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
-
         binding = TodoAddBinding.inflate(inflater, container, false);
         todosBinding = ListTodosBinding.inflate(inflater, container, false);
         return binding.getRoot();
@@ -40,12 +85,19 @@ public class AddTodo extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        init();
+        init(savedInstanceState);
         setListeners();
     }
 
-    private void init () {
-        // Get the recycler view adapter
+    private void init (Bundle savedInstanceState) {
+
+        // initialize the Location Manager:
+        locationManager = (LocationManager) App.mContext.getSystemService(Context.LOCATION_SERVICE);
+        mapView = binding.maps;
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this::mapView_onMapReady);
+
+        // Get the recycler view adapter:
         todosList = new ArrayList<>();
         adapter = (TodosRecyclerAdapter) todosBinding.recyclerView.getAdapter();
     }
@@ -78,10 +130,91 @@ public class AddTodo extends Fragment {
         });
     }
 
+    private void initMap() {
+        if (ContextCompat.checkSelfPermission(App.mContext, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(binding.maps.getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            if (googleMap != null) {
+                googleMap.setMyLocationEnabled(true);
+                googleMap.getUiSettings().setMyLocationButtonEnabled(true);
+                googleMap.getUiSettings().setAllGesturesEnabled(true);
+                googleMap.getUiSettings().setZoomControlsEnabled(true);
+            }
+        } else {
+            if (ContextCompat.checkSelfPermission(App.mContext, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this.getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 12);
+            }
+            if (ContextCompat.checkSelfPermission(App.mContext, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this.getActivity(), new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 13);
+            }
+        }
+    }
+
+    private void getCurrentLocation() {
+        if (ContextCompat.checkSelfPermission(App.mContext, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(App.mContext, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            boolean isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+            boolean isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+            if (!isGPSEnabled && !isNetworkEnabled) {
+                Toast.makeText(App.mContext, getText(R.string.provider_failed), Toast.LENGTH_LONG).show();
+            } else {
+                location = null;
+                if (isGPSEnabled) {
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, LOCATION_MIN_UPDATE_TIME, LOCATION_MIN_UPDATE_DISTANCE, locationListener);
+                    location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                }
+                if (isNetworkEnabled) {
+                    locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, LOCATION_MIN_UPDATE_TIME, LOCATION_MIN_UPDATE_DISTANCE, locationListener);
+                    location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                }
+                if (location != null) {
+                    drawMarker(location, getText(R.string.your_location).toString());
+                }
+            }
+        } else {
+            if (ContextCompat.checkSelfPermission(App.mContext, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this.getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 12);
+            }
+            if (ContextCompat.checkSelfPermission(App.mContext, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this.getActivity(), new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 13);
+            }
+        }
+    }
+
+    private void mapView_onMapReady(GoogleMap googleMap) {
+        this.googleMap = googleMap;
+        initMap();
+        getCurrentLocation();
+    }
+
+    private void drawMarker(Location location, String title) {
+        if (this.googleMap != null) {
+            googleMap.clear();
+            LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+            MarkerOptions markerOptions = new MarkerOptions();
+            markerOptions.position(latLng);
+            markerOptions.title(title);
+            markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
+            googleMap.addMarker(markerOptions);
+            googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+            googleMap.animateCamera(CameraUpdateFactory.zoomTo(12));
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+        getCurrentLocation();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+        mapView.onDestroy();
     }
-
 }
